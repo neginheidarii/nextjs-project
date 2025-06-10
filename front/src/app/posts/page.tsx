6bb08c3d938715a99760d870e817a506d180f4a3
neginheidarii <neginheidari@icloud.com>
2025-06-10 15:23:54 -0400

    style: format code in PostPage component for consistency

--- front/src/app/posts/page.tsx
+++ front/src/app/posts/page.tsx
@@ -1,25 +1,24 @@
-"use client"
-import { usePosts } from "@/hooks/usePosts"
+"use client";
+import { usePosts } from "@/hooks/usePosts";
 
-export default function PostPage(){
-    const {data, isLoading, error} = usePosts();
+export default function PostPage() {
+  const { data, isLoading, error } = usePosts();
 
-    if(isLoading) return <p> Loading ... </p>;
-    if(error) return <p>Error loading posts: {(error as Error).message}</p>
+  if (isLoading) return <p> Loading ... </p>;
+  if (error) return <p>Error loading posts: {(error as Error).message}</p>;
+  console.log("fetched posts: ", data)
 
-    return(
-        <div>
-            <h1>Posts</h1>
-            <ul>
-                {data.map((post: any)=>(
-                    <li key={post.id}>
-                        <strong>{post.title}</strong>
-                        <p>{post.body}</p>
-                    </li>
-                ))}
-            </ul>
-            
-        </div>
-    )
-    
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post: any) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
