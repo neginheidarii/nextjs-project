698faba26de6525e0a7d18bd52cf946f00ba4aed
neginheidarii <neginheidari@icloud.com>
2025-06-16 11:50:22 -0400

    fix: improve error handling and logging in fetchComments function

--- front/src/hooks/useComments.ts
+++ front/src/hooks/useComments.ts
@@ -1,17 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchComments = async(postId: string)=>{
-    const response = await axios.get(
-        `http://localhost:3000/comments?postId=${postId}`
+const fetchComments = async (postId: string) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/comments?postId=${postId}`);
+    console.log("API response data for comments:", response.data);
+    return response.data;
+  } catch (err) {
+    console.error("Error fetching comments:", err);
+    throw err;
+  }
+};
 
-    )
-    return response.data
-}
 console.log("fetchComments", fetchComments)
 
 
-export const useComments= (postId:string)=>{
+export const useComments= (postId: string)=>{
     return useQuery ({
         queryKey: ['comments', postId],
         queryFn: ()=> fetchComments(postId),
